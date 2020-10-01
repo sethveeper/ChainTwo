7484a2b295926df5811cb3d3110c1b8a1ef5fbfd
Dominic Jones-Jackson <sethveeper@gmail.com>
2020-10-01 10:19:56 -0500

    Project initialized!

---
+++ ChainTwo/Chain.cs
@@ -0,0 +1,19 @@
+﻿/*
+    Author: Dominic "Seth" Jones-Jackson (F)
+    E-mail: sethveeper@gmail.com
+    
+    Initialized on: October 1, 2020
+    
+ */
+
+using System;
+
+namespace ChainTwo
+{
+    public class Chain
+    {
+
+    }
+    // End of Chain class
+}
+// End of namespace
---
+++ ChainTwo/Link.cs
@@ -0,0 +1,21 @@
+﻿/*
+    Author: Dominic "Seth" Jones-Jackson (F)
+    E-mail: sethveeper@gmail.com
+    
+    Initialized on: October 1, 2020
+    
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+// End of using statements
+
+namespace ChainTwo
+{
+    class Link
+    {
+    }
+    // End of Link class
+}
+// End of namespace
